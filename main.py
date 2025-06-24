e38dbcb6bd9b89af93edb536e517883094dbd2d7
Orion Storm <orionstorm95@gmail.com>
2025-06-24 19:31:27 -0400

    switched to argv, added printout

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from stats import print_pretty_book_stats
+import sys
 
 def main():
-    filepath = "books/frankenstein.txt"
-    print_pretty_book_stats(filepath)
+    
+    num_args = len(sys.argv)
+
+    if num_args == 1 or num_args > 2:
+      print("Usage: python3 main.py <path_to_book>")
+      sys.exit(1)
+
+    print_pretty_book_stats(sys.argv[1])
+    
+   # filepath = "books/frankenstein.txt"
+   # print_pretty_book_stats(filepath)
 
 
 main()
